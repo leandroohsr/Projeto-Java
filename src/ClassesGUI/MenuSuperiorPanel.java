db61cb1d96833274d4005b0869c56686ed1ef2a2
eoleleu <l213437@dac.unicamp.br>
2023-06-20 19:01:57 -0300

    botao salvar

--- src/ClassesGUI/MenuSuperiorPanel.java
+++ src/ClassesGUI/MenuSuperiorPanel.java
@@ -2,11 +2,15 @@ package ClassesGUI;
 
 import javax.swing.*;
 import ClassesJogo.*;
-import Construcoes.Habitavel;
-import Construcoes.Rentavel;
+import ClassesJogo.Excepts.ExceptionLackOfMoney;
+import Construcoes.Casa;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MenuSuperiorPanel extends JPanel {
     Cidade cidade;
@@ -15,17 +19,54 @@ public class MenuSuperiorPanel extends JPanel {
     public MenuSuperiorPanel(Cidade cidade) {
         this.cidade = cidade;
         JButton b = new JButton("botao cima");
+        JButton salvar = new JButton("Salvar");
 
 
-        box.addItem(0);
-        box.addItem("bb");
-
-        box.setBounds(50,50,90,20);
-        add(box);
-
+//        box.addItem(0);
+//        box.addItem("bb");
+//
+//        box.setBounds(50,50,90,20);
+//        add(box);
+        add(salvar);
+        salvar.addActionListener(new salvaJogo());
         add(b);
 
         setVisible(true);
+    }
+
+
+
+    static class salvaJogo implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            try {
+
+                FileWriter fw = new FileWriter("src/ClassesJogo/Jogos.csv", true);
+                BufferedWriter out = new BufferedWriter(fw);
+                out.write("test");
+                out.newLine();
+                out.close();
+
+                Font fonte_padrao = new Font("Arial", Font.PLAIN, 50);
+                JDialog dialog = new JDialog();
+                dialog.setBounds(400,300,300,150);
+                JLabel labelErro = new JLabel("Jogo salvo!");
+                labelErro.setFont(fonte_padrao);
+                dialog.add(labelErro);
+                dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                dialog.setVisible(true);
+
+            } catch (IOException erro) {
+                Font fonte_padrao = new Font("Arial", Font.PLAIN, 50);
+                JDialog dialog = new JDialog();
+                dialog.setBounds(400,300,300,150);
+                JLabel labelErro = new JLabel("O jogo não foi salvo!");
+                labelErro.setFont(fonte_padrao);
+                dialog.add(labelErro);
+                dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                dialog.setVisible(true);
+            }
+        }
     }
 
 
